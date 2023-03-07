08093af103e979842785869b167601bdfd7cd77e
Svitla <Svitla.m@gmail.com>
2023-03-07 23:51:22 +0200

    add base html

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import Weather from './Weather';
 export default function App() {
   return (
     <div className="Weather App">
-      <h1>
-      Weather application
-      </h1>
+      <div className='container'>
+      <Weather/>
      
 <footer> 
   This project was coded by Svitlana Dolinta and is
       <a href="https://github.com/SvitlaM/weather-application" target="_blank"> open sourced on GitHub</a>
-      <Weather/>
+      
       </footer>
+      </div>
+  
     </div>
   );
 }
