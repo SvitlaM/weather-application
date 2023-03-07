e9a51cf094df57c19f7d1fda5107a105cf4801b6
Svitla <Svitla.m@gmail.com>
2023-03-07 13:33:17 +0200

    add the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@
 import './App.css';
 import Weather from './Weather';
 
-function App() {
+export default function App() {
   return (
     <div className="Weather App">
+      <h1>
       Weather application
+      </h1>
+     
+<footer> 
+  This project was coded by Svitlana Dolinta and is
+      <a href="https://github.com/SvitlaM/weather-application" target="_blank"> open sourced on GitHub</a>
       <Weather/>
+      </footer>
     </div>
   );
 }
 
-export default App;
+ 
